9f34340073451771252bfb0b26058f7b265a2f5f
Thomas Chamberlin <tchamber@nrao.edu>
2019-08-23 13:11:06 -0400

    Proper FKs, Poetry, admin tweaks

--- gbtarchive_orm/settings.py
+++ gbtarchive_orm/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+*_y2$h_5svzz3n36*g3m$(vzuy605j)&29sylo9u$=ol^qn5c'
+SECRET_KEY = "+*_y2$h_5svzz3n36*g3m$(vzuy605j)&29sylo9u$=ol^qn5c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,45 +31,45 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'metadata'
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "metadata",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'gbtarchive_orm.urls'
+ROOT_URLCONF = "gbtarchive_orm.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ]
         },
-    },
+    }
 ]
 
-WSGI_APPLICATION = 'gbtarchive_orm.wsgi.application'
+WSGI_APPLICATION = "gbtarchive_orm.wsgi.application"
 
 
 # Database
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'gbtarchive_orm.wsgi.application'
 
 user = "tchamber"
 DATABASES = {
-    'default': {
+    "default": {
         "ENGINE": "django.db.backends.mysql",
         "NAME": f"gbtarchive_{user}_dev",
         "USER": user,
@@ -90,26 +90,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -121,4 +115,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
--- metadata/models.py
+++ metadata/models.py
@@ -9,147 +9,314 @@ from django.db import models
 
 
 class Observer(models.Model):
-    observerid = models.AutoField(db_column='observerID', primary_key=True)  # Field name made lowercase.
+    observerid = models.AutoField(db_column="observerID", primary_key=True)
     name = models.CharField(max_length=64)
 
     class Meta:
         managed = False
-        db_table = 'Observer'
+        db_table = "Observer"
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Coordinates(models.Model):
-    coordinateid = models.AutoField(db_column='coordinateID', primary_key=True)  # Field name made lowercase.
-    ra = models.FloatField(db_column='RA')  # Field name made lowercase.
-    dec = models.FloatField(db_column='DEC')  # Field name made lowercase.
-    coordinatesystem = models.CharField(db_column='coordinateSystem', max_length=64)  # Field name made lowercase.
+    coordinateid = models.AutoField(db_column="coordinateID", primary_key=True)
+    ra = models.FloatField(db_column="RA")
+    dec = models.FloatField(db_column="DEC")
+    coordinatesystem = models.CharField(
+        db_column="coordinateSystem",
+        max_length=64,
+        blank=True,
+        choices=(
+            ("RADEC", "RADEC"),
+            ("OTHER", "OTHER"),
+            ("GALACTIC", "GALACTIC"),
+            ("AZEL", "AZEL"),
+            ("HADEC", "HADEC"),
+            ("ENCODERAZEL", "ENCODERAZEL"),
+            ("", "Blank"),
+        ),
+    )
+    # TODO: Always 0; delete?
     equinox = models.IntegerField()
+    # TODO: Always 0; delete?
     radesys = models.IntegerField()
+    # TODO: Always 0; delete?
     planetary = models.IntegerField()
+    # Used
     obsaz = models.FloatField()
+    # Used
     obsel = models.FloatField()
-    raj2000 = models.FloatField(db_column='RAJ2000')  # Field name made lowercase.
-    decj2000 = models.FloatField(db_column='DECJ2000')  # Field name made lowercase.
+    # Used
+    raj2000 = models.FloatField(db_column="RAJ2000")
+    # Used
+    decj2000 = models.FloatField(db_column="DECJ2000")
 
     class Meta:
         managed = False
-        db_table = 'coordinates'
+        db_table = "coordinates"
 
 
 class Error(models.Model):
-    errorid = models.AutoField(db_column='errorID', primary_key=True)  # Field name made lowercase.
-    errormsg = models.CharField(db_column='errorMsg', max_length=64)  # Field name made lowercase.
+    errorid = models.AutoField(db_column="errorID", primary_key=True)
+    errormsg = models.CharField(db_column="errorMsg", max_length=64)
     severity = models.IntegerField()
 
     class Meta:
         managed = False
-        db_table = 'error'
+        db_table = "error"
+
+    def __str__(self):
+        return f"{self.severity}: {self.errormsg}"
 
 
 class File(models.Model):
-    fileid = models.AutoField(db_column='fileID', primary_key=True)  # Field name made lowercase.
+    fileid = models.AutoField(db_column="fileID", primary_key=True)
     name = models.CharField(max_length=256)
     date = models.DateTimeField()
     size = models.IntegerField()
 
     class Meta:
         managed = False
-        db_table = 'file'
+        db_table = "file"
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class History(models.Model):
-    historyid = models.AutoField(db_column='historyID', primary_key=True)  # Field name made lowercase.
-    archivaldate = models.DateField(db_column='archivalDate')  # Field name made lowercase.
-    aatfilename = models.CharField(db_column='aatFilename', max_length=256)  # Field name made lowercase.
+    historyid = models.AutoField(db_column="historyID", primary_key=True)
+    archivaldate = models.DateField(db_column="archivalDate")
+    aatfilename = models.CharField(db_column="aatFilename", max_length=256)
     version = models.CharField(max_length=12)
 
     class Meta:
         managed = False
-        db_table = 'history'
+        db_table = "history"
+        verbose_name_plural = "Histories"
 
 
-class Obsparameter(models.Model):
-    obsparameterid = models.AutoField(db_column='obsParameterID', primary_key=True)  # Field name made lowercase.
-    backend = models.CharField(max_length=64)
-    receiver = models.CharField(max_length=1024)
+class ObsParameter(models.Model):
+    obsparameterid = models.AutoField(db_column="obsParameterID", primary_key=True)
+    backend = models.CharField(
+        max_length=64,
+        choices=(
+            ("", "UNKNOWN"),
+            ("CCB26_40", "CCB26_40"),
+            ("DCR", "DCR"),
+            ("DCR, CCB26_40", "DCR, CCB26_40"),
+            ("DCR, SpectralProcessor", "DCR, SpectralProcessor"),
+            ("DCR, Spectrometer", "DCR, Spectrometer"),
+            ("GUPPI", "GUPPI"),
+            ("MUSTANG", "MUSTANG"),
+            ("SpectralProcessor", "SpectralProcessor"),
+            ("SpectralProcessor, CCB26_40", "SpectralProcessor, CCB26_40"),
+            ("SpectralProcessor, DCR", "SpectralProcessor, DCR"),
+            ("Spectrometer", "Spectrometer"),
+            ("Spectrometer, DCR", "Spectrometer, DCR"),
+            ("Spectrometer, SpectralProcessor", "Spectrometer, SpectralProcessor"),
+            ("SPIGOT", "SPIGOT"),
+            ("VEGAS", "VEGAS"),
+            ("VLBA_DAR", "VLBA_DAR"),
+            ("Zpectrometer", "Zpectrometer"),
+        ),
+    )
+    receiver = models.CharField(
+        max_length=1024,
+        blank=True,
+        choices=(
+            ("", "UNKNOWN"),
+            ("Holography", "Holography"),
+            ("Rcvr12_18", "Rcvr12_18"),
+            ("Rcvr18_26", "Rcvr18_26"),
+            ("Rcvr1_2", "Rcvr1_2"),
+            ("Rcvr26_40", "Rcvr26_40"),
+            ("Rcvr2_3", "Rcvr2_3"),
+            ("Rcvr40_52", "Rcvr40_52"),
+            ("Rcvr4_6", "Rcvr4_6"),
+            ("Rcvr68_92", "Rcvr68_92"),
+            ("Rcvr8_10", "Rcvr8_10"),
+            ("Rcvr_342", "Rcvr_342"),
+            ("Rcvr_800", "Rcvr_800"),
+            ("Rcvr_PAR", "Rcvr_PAR"),
+            ("RcvrArray18_26", "RcvrArray18_26"),
+            ("RcvrPF_1", "RcvrPF_1"),
+            ("RcvrPF_2", "RcvrPF_2"),
+        ),
+    )
     nchan = models.CharField(max_length=1024)
     bandwidth = models.CharField(max_length=1024)
     velocity = models.FloatField()
-    velocitydef = models.CharField(db_column='velocityDef', max_length=64)  # Field name made lowercase.
+    velocitydef = models.CharField(db_column="velocityDef", max_length=64)
     restfreq = models.CharField(max_length=1024)
-    poln = models.CharField(max_length=64)
+    poln = models.CharField(
+        max_length=64,
+        blank=True,
+        choices=(
+            ("XX,YY", "XX,YY"),
+            ("", "Blank"),
+            ("LL,RR", "LL,RR"),
+            ("LL,RR,LR,RL", "LL,RR,LR,RL"),
+            ("XX,YY,XY,YX", "XX,YY,XY,YX"),
+            ("UNKNOWN", "UNKNOWN"),
+            ("LL", "LL"),
+            ("I", "I"),
+            ("I,Q,U,V", "I,Q,U,V"),
+        ),
+    )
     poln_num = models.CharField(max_length=64)
     mode = models.CharField(max_length=64)
-    recband = models.CharField(db_column='recBand', max_length=12)  # Field name made lowercase.
+    recband = models.CharField(
+        db_column="recBand",
+        max_length=12,
+        choices=(
+            ("L", "L"),
+            ("Unknown", "Unknown"),
+            ("X", "X"),
+            ("C", "C"),
+            ("K", "K"),
+            ("U", "U"),
+            ("P", "P"),
+            ("S", "S"),
+            ("Q", "Q"),
+            ("Ka", "Ka"),
+            ("W", "W"),
+        ),
+    )
 
     class Meta:
         managed = False
-        db_table = 'obsParameter'
+        db_table = "obsParameter"
+        verbose_name = "Obs. Parameter"
+
+    def __str__(self):
+        return f"{self.backend if self.backend else 'UNKNOWN'} : {self.receiver if self.receiver else 'UNKNOWN'}"
 
 
-class Obsprocedure(models.Model):
-    obsprocedureid = models.AutoField(db_column='obsProcedureID', primary_key=True)  # Field name made lowercase.
+class ObsProcedure(models.Model):
+    obsprocedureid = models.AutoField(db_column="obsProcedureID", primary_key=True)
     name = models.CharField(max_length=64)
-    type = models.CharField(max_length=64)
-    obstype = models.CharField(db_column='obsType', max_length=64)  # Field name made lowercase.
+    type = models.CharField(
+        max_length=64,
+        choices=(
+            ("POINTING", "POINTING"),
+            ("SIMPLE", "SIMPLE"),
+            ("CALIBRATION", "CALIBRATION"),
+            ("MAP", "MAP"),
+            ("UNKNOWN", "UNKNOWN"),
+            ("OOFMAP", "OOFMAP"),
+        ),
+    )
+    obstype = models.CharField(
+        db_column="obsType",
+        max_length=64,
+        choices=[
+            ("CONTINUUM", "CONTINUUM"),
+            ("LINE", "LINE"),
+            ("PTIMING", "PTIMING"),
+            ("UNKNOWN", "UNKNOWN"),
+            ("PSEARCH", "PSEARCH"),
+            ("PMONITOR", "PMONITOR"),
+            ("Radar", "Radar"),
+            ("Pulsar", "Pulsar"),
+            ("VLBI", "VLBI"),
+            ("PCAL", "PCAL"),
+        ],
+    )
     procscan = models.CharField(max_length=64)
 
     class Meta:
         managed = False
-        db_table = 'obsProcedure'
+        db_table = "obsProcedure"
+        verbose_name = "Obs. Procedure"
+
+    def __str__(self):
+        return f"{self.name} ({self.type}/{self.obstype})"
 
 
 class Project(models.Model):
-    projectid = models.AutoField(db_column='projectID', primary_key=True)  # Field name made lowercase.
+    projectid = models.AutoField(db_column="projectID", primary_key=True)
+    # Not unique
     propname = models.CharField(max_length=64)
-    name = models.CharField(max_length=64)
+    name = models.CharField(max_length=64, unique=True)
 
     class Meta:
         managed = False
-        db_table = 'project'
+        db_table = "project"
+
+    def __str__(self):
+        return f"{self.name}"
 
 
 class Scan(models.Model):
-    scanid = models.AutoField(db_column='scanID', primary_key=True)  # Field name made lowercase.
+    scanid = models.AutoField(db_column="scanID", primary_key=True)
     number = models.IntegerField()
     object = models.CharField(max_length=64)
-    obsidentifier = models.CharField(db_column='obsIdentifier', max_length=64)  # Field name made lowercase.
-    projectid = models.IntegerField(db_column='projectID')  # Field name made lowercase.
-    observerid = models.IntegerField(db_column='observerID')  # Field name made lowercase.
-    obsprocedureid = models.IntegerField(db_column='obsProcedureID')  # Field name made lowercase.
-    obsparameterid = models.IntegerField(db_column='obsParameterID')  # Field name made lowercase.
-    coordinateid = models.IntegerField(db_column='coordinateID')  # Field name made lowercase.
-    errorid = models.IntegerField(db_column='errorID')  # Field name made lowercase.
-    historyid = models.IntegerField(db_column='historyID')  # Field name made lowercase.
-    fileid = models.IntegerField(db_column='fileID')  # Field name made lowercase.
-    sessionid = models.IntegerField(db_column='sessionID')  # Field name made lowercase.
-    dateobserved = models.DateTimeField(db_column='dateObserved')  # Field name made lowercase.
-    integrationtime = models.FloatField(db_column='integrationTime')  # Field name made lowercase.
+    obsidentifier = models.CharField(db_column="obsIdentifier", max_length=64)
+    project = models.ForeignKey(
+        "Project", db_column="projectID", on_delete=models.CASCADE
+    )
+    observer = models.ForeignKey(
+        "Observer", db_column="observerID", on_delete=models.CASCADE
+    )
+    obsprocedure = models.ForeignKey(
+        "ObsProcedure", db_column="obsProcedureID", on_delete=models.CASCADE
+    )
+    obsparameter = models.ForeignKey(
+        "ObsParameter", db_column="obsParameterID", on_delete=models.CASCADE
+    )
+    coordinate = models.ForeignKey(
+        "Coordinates", db_column="coordinateID", on_delete=models.CASCADE
+    )
+    # NOTE: 0 is used as NULL here
+    error = models.ForeignKey("Error", db_column="errorID", on_delete=models.CASCADE)
+    history = models.ForeignKey(
+        "History", db_column="historyID", on_delete=models.CASCADE
+    )
+    file = models.OneToOneField("File", db_column="fileID", on_delete=models.CASCADE)
+    session = models.ForeignKey(
+        "Session", db_column="sessionID", on_delete=models.CASCADE
+    )
+    dateobserved = models.DateTimeField(db_column="dateObserved")
+    integrationtime = models.FloatField(db_column="integrationTime")
     scanlength = models.IntegerField()
     archived = models.IntegerField()
 
     class Meta:
         managed = False
-        db_table = 'scan'
+        db_table = "scan"
+
+    def __str__(self):
+        return (
+            f"Scan {self.number} of '{self.obsidentifier}' "
+            f"('{self.object}') [{self.scanlength}s]"
+        )
 
 
 class Session(models.Model):
-    sessionid = models.AutoField(db_column='sessionID', primary_key=True)  # Field name made lowercase.
-    projectid = models.IntegerField(db_column='projectID')  # Field name made lowercase.
+    sessionid = models.AutoField(db_column="sessionID", primary_key=True)
+    project = models.ForeignKey(
+        "Project", db_column="projectID", on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=64)
-    gofitsversion = models.CharField(db_column='GOFitsVersion', max_length=64)  # Field name made lowercase.
-    msgflag = models.CharField(db_column='MSGFLAG', max_length=1024)  # Field name made lowercase.
-    msglevel = models.CharField(db_column='MSGLEVEL', max_length=4)  # Field name made lowercase.
+    gofitsversion = models.CharField(db_column="GOFitsVersion", max_length=64)
+    msgflag = models.CharField(db_column="MSGFLAG", max_length=1024)
+    msglevel = models.CharField(db_column="MSGLEVEL", max_length=4)
 
     class Meta:
         managed = False
-        db_table = 'session'
+        db_table = "session"
+
+    def __str__(self):
+        return f"Session {self.name}"
 
 
-class TestOfflineOld(models.Model):
-    errorid = models.AutoField(db_column='errorID', primary_key=True)  # Field name made lowercase.
-    errormsg = models.CharField(db_column='errorMsg', max_length=64)  # Field name made lowercase.
-    severity = models.IntegerField()
+# class TestOfflineOld(models.Model):
+#     errorid = models.AutoField(db_column="errorID", primary_key=True)
+#     errormsg = models.CharField(db_column="errorMsg", max_length=64)
+#     severity = models.IntegerField()
 
-    class Meta:
-        managed = False
-        db_table = 'test_offline_old'
+#     class Meta:
+#         managed = False
+#         db_table = "test_offline_old"
